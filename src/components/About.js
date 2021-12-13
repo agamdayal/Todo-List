8181f1c522cac57f497d00f014693b04fd79cee9
Agam Dayal <68564756+agamdayal@users.noreply.github.com>
2021-12-14 01:50:15 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const About = () => {
     return(
-        <h1>Hello</h1>
+        <>
+        <h3>TODO LIST</h3>
+        <p> This is a Todo List for you.</p>
+        <p>This helps you to update your list so you never forget any of your important tasks!</p>
+        </>
     )
 }
 
